37f5899f52116dbac09585742a940b6a236c2f72
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-03-12 12:12:29 +0000

    deployment

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get("/", (req, res) => {
  res.status(200).send(`<div style="text-align:center">
  
  <h1>Hall booking api's</h1>
- 
+       <h5><a href="https://web.postman.co/workspace/0b740ab5-96dc-4a8f-85b2-10a7807d7fb9/collection/33139018-fcecaddf-775a-4679-9261-5fb62dd2ab42?action=share&source=copy-link&creator=33139018">Postman Collection Link</a></h5>
+
  </div>`);
  
 });
