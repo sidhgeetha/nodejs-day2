1449793c6eaea09d30e97ef70b98e53914265ccb
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-03-12 11:46:19 +0000

    deployment chnages

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  // res.status(200).json({message:"Hi team,welcome to guvi world"})
+  res.status(200).json({message:"Hall booking api's"})
   res
     .status(200)
     .send(
-      `<div style="background-color:yellow;"> <h1>https://github.com/sidhgeetha/nodejs-day2.git</h1> </div>`
-      `<div style="background-color:yellow;"> <h1>https://web.postman.co/workspace/0b740ab5-96dc-4a8f-85b2-10a7807d7fb9/request/33139018-e9e4d59b-7f88-46d9-b33a-e440414c2e52</h1> </div>`
+      `<div > <h1>https://github.com/sidhgeetha/nodejs-day2.git</h1> </div>`
     );
 });
 
