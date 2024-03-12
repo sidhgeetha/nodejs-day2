139cc139d566081de5e87809473650f081e79d5c
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-03-12 11:41:27 +0000

    deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     .status(200)
     .send(
       `<div style="background-color:yellow;"> <h1>https://github.com/sidhgeetha/nodejs-day2.git</h1> </div>`
-    //   `<div style="background-color:yellow;"> <h1>https://web.postman.co/workspace/0b740ab5-96dc-4a8f-85b2-10a7807d7fb9/request/33139018-e9e4d59b-7f88-46d9-b33a-e440414c2e52</h1> </div>`
+      `<div style="background-color:yellow;"> <h1>https://web.postman.co/workspace/0b740ab5-96dc-4a8f-85b2-10a7807d7fb9/request/33139018-e9e4d59b-7f88-46d9-b33a-e440414c2e52</h1> </div>`
     );
 });
 
